afad59c1f010475560bc31fd69b790145ed2ea3f
Robert Bishop <rbishop85@gmail.com>
2023-02-06 22:01:11 -0800

    started work on some query resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,20 +1,31 @@
 const { AuthenticationError } = require('apollo-server-express');
-const { User } = require('../models');
+const { User, Vehicle, Update } = require('../models');
 const { signToken } = require('../utils/auth');
 
 const resolvers = {
   Query: {
+    // Find all users
     users: async () => {
       return User.find();
     },
+    // Find single user by username
     user: async (parent, { username }) => {
       return User.findOne({ username });
     },
+    // If logged in, search current user
     me: async (parent, args, context) => {
       if (context.user) {
         return User.findOne({ _id: context.user._id });
       }
       throw new AuthenticationError('You need to be logged in!');
+    },
+    // Find all vehicles
+    vehicles: async () => {
+      return Vehicle.find();
+    },
+    // Find single vehicle by ID
+    vehicle: async (parent, { _id }) => {
+      return Vehicle.findOne({ _id });
     },
   },
 
@@ -24,11 +35,11 @@ const resolvers = {
       const token = signToken(user);
       return { token, user };
     },
-    login: async (parent, { email, password }) => {
-      const user = await User.findOne({ email });
+    login: async (parent, { username, password }) => {
+      const user = await User.findOne({ username });
 
       if (!user) {
-        throw new AuthenticationError('No user found with this email address');
+        throw new AuthenticationError('No user found with this username');
       }
 
       const correctPw = await user.isCorrectPassword(password);
