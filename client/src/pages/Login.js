ac4347004686c3185ec8dc53543f3528a8972bce
Robert Bishop <rbishop85@gmail.com>
2023-02-20 20:25:27 -0800

    set login page to reroute to homepage if already logged in

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, Navigate } from 'react-router-dom';
 import { useMutation } from '@apollo/client';
 import { LOGIN_USER } from '../utils/mutations';
 
@@ -41,54 +41,65 @@ const Login = (props) => {
   };
 
   return (
-    <main className="flex-row justify-center mb-4">
-      <div className="col-12 col-lg-10">
-        <div className="card">
-          <h4 className="card-header bg-dark text-light p-2">Login</h4>
-          <div className="card-body">
-            {data ? (
-              <p>
-                Success! You may now head{' '}
-                <Link to="/">back to the homepage.</Link>
-              </p>
-            ) : (
-              <form onSubmit={handleFormSubmit}>
-                <input
-                  className="form-input"
-                  placeholder="Your email"
-                  name="email"
-                  type="email"
-                  value={formState.email}
-                  onChange={handleChange}
-                />
-                <input
-                  className="form-input"
-                  placeholder="******"
-                  name="password"
-                  type="password"
-                  value={formState.password}
-                  onChange={handleChange}
-                />
-                <button
-                  className="btn btn-block btn-primary"
-                  style={{ cursor: 'pointer' }}
-                  type="submit"
-                >
-                  Submit
-                </button>
-              </form>
-            )}
+    <div>
+      {!Auth.loggedIn() ? (
+        <>
+          <div>
+            <main className="flex-row justify-center mb-4">
+              <div className="col-12 col-lg-10">
+                <div className="card">
+                  <h4 className="card-header bg-dark text-light p-2">Login</h4>
+                  <div className="card-body">
+                    {data ? (
+                      <p>
+                        Success! You may now head{' '}
+                        <Link to="/">back to the homepage.</Link>
+                      </p>
+                    ) : (
+                      <form onSubmit={handleFormSubmit}>
+                        <input
+                          className="form-input"
+                          placeholder="Your email"
+                          name="email"
+                          type="email"
+                          value={formState.email}
+                          onChange={handleChange}
+                        />
+                        <input
+                          className="form-input"
+                          placeholder="******"
+                          name="password"
+                          type="password"
+                          value={formState.password}
+                          onChange={handleChange}
+                        />
+                        <button
+                          className="btn btn-block btn-primary"
+                          style={{ cursor: 'pointer' }}
+                          type="submit"
+                        >
+                          Submit
+                        </button>
+                      </form>
+                    )}
 
-            {error && (
-              <div className="my-3 p-3 bg-danger text-white">
-                {error.message}
+                    {error && (
+                      <div className="my-3 p-3 bg-danger text-white">
+                        {error.message}
+                      </div>
+                    )}
+                  </div>
+                </div>
               </div>
-            )}
+            </main>
           </div>
-        </div>
-      </div>
-    </main>
+        </>
+      ) : (
+        <Navigate to="/" />
+      )}
+    </div>
   );
+
 };
 
 export default Login;
